17a763dd7d94a196fc0a494a34aeb1ae21aba96c
mourin <mourin@10.211.55.3>
2015-03-31 16:41:42 +0800

    弱视治疗目录

---
+++ src/com/xmdx/demo/patient/service/AmblyopiaCureService.java
@@ -0,0 +1,53 @@
+package com.xmdx.demo.patient.service;
+
+import com.e9rj.platform.common.services.BusinessServices;
+import com.xmzy.frameext.business.service.annotate.Service;
+import com.xmzy.framework.context.ActionContext;
+@Service(name="patient.amb")
+public class AmblyopiaCureService extends BusinessServices {
+	//功能号
+	private static final String authFuncNo = "patient.amb";
+	//表名
+	private static final String tableName = "TB_USER";
+	//主键名
+	private static final String keyField = "U_ID";
+	@Override
+	public int delete(ActionContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int goTo(ActionContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int init(ActionContext ac) throws Exception {
+		checkAuth(ac, authFuncNo, RIGHT_ONE);
+		
+		ac.setStringValue("tabLogo", authFuncNo);
+		ac.setStringValue(CONST_FORMNAME, "com/xmdx/demo/patient/amblyopia_cure.html");		
+		return CONST_RESULT_SUCCESS;
+	}
+
+	@Override
+	public int insertExportData(ActionContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int query(ActionContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int save(ActionContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
